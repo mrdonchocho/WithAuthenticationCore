77cb559cacf3a40c95e08abde8cd7043bd7ddf5b
Donald Igbeta <digbeta@diamondbank.com>
2020-07-20 10:11:09 +0100

    Add project files.

---
+++ WithAuthenticationCore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WithAuthenticationCore.Data;
+
+[assembly: HostingStartup(typeof(WithAuthenticationCore.Areas.Identity.IdentityHostingStartup))]
+namespace WithAuthenticationCore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ WithAuthenticationCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WithAuthenticationCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
